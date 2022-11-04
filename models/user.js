f1c271b3e3099a1aec86b03bbc0da0d7d7031d36
Ayush Goyal <93733866+Ayush-207@users.noreply.github.com>
2022-11-04 23:35:48 +0530

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const User = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    college: String,
+    registeredEvents: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Event'
+        }
+    ],
+    hostedEvents: [{
+
+        type: Schema.Types.ObjectId,
+        ref: 'Event'
+
+    }]
+})
+
+User.plugin(passportLocalMongoose);
+module.exports = mongoose.model('User', User);
